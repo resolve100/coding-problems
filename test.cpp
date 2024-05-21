92f5b643cd0e7d0e66e92842d3bb63b98fe995ba
resolve100 <iisbaitan.gen@gmail.com>
2024-05-22 00:07:56 +0300

    update test

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned ll
+#define pb push_back
+#define int long long
+#define f first
+#define s second
+#define pq priority_queue
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define read_file freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+
+using namespace std;
+
+void solve(){
+
+}
+
+int32_t main() {
+    fast;
+
+    int t; cin>>t;
+    while (t--){
+        solve();
+    }
+    return 0;
+}
